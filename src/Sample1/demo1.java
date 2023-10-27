cc20126264980462ce5fa44e1ebadc35bac0de46
Admin <Admin@192.168.1.5>
2023-10-28 00:24:28 +0530

    [Suraj]: added new Project

--- src/Sample1/demo1.java
+++ src/Sample1/demo1.java
@@ -1,5 +1,40 @@
 package Sample1;
 
-public class demo1 {
+public class demo1 
+{
+	public static void main(String[] args) 
+	{
+		//*****
+		//****
+		//***
+		//**
+		//*
+		//**
+		//***
+		//****
+		//*****
+		
+		int star=5;
+		
+		for (int i=1; i<=9; i++)
+		{
+			for(int j=1; j<=star; j++)
+			{
+				System.out.print("*");
+			}
+			
+			System.out.println();
+			
+			if(i<5)
+			{
+				star--;
+			}
+			else
+			{
+				star++;
+			}
+		}
+		
+	}
 
 }
